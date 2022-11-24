b0437c01dc19ca930283d8d90b943c6d84b4c02a
James Murphy <jamesivdeveloper@gmail.com>
2022-11-23 18:26:53 -0600

    add style

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends Component {
         <SearchBox
           onChangeHandler={onSearchChange}
           placeholder="search monsters"
-          className="search-box"
+          className="monsters-search-box"
         />
         <CardList monsters={filteredMonsters} />
       </div>
